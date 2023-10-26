2e42c61606a5851494c54a440a075aead7b39932
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-10-26 18:19:49 -0500

    added resume and link works

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,22 +1,15 @@
+const containerStyle = {
+marginTop: '6rem',
+};
+
+
 export default function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+    <>
+    <div className="d-flex flex-column justify-content-center align-items-center" style={containerStyle}>
+    <h1>Take a look at my resume</h1>
+    <a href="https://docs.google.com/document/d/145LhjsofgCcH4tA8GYnqLCev5zuGpMf2j0ypLNViWoc/edit?usp=sharing" target="blank">Link To resume</a>
     </div>
+    </>
   );
 }
