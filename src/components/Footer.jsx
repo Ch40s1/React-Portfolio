6c0562adc38ca78005a1371c1bbcdcdd5e66d0ec
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-10-27 13:15:19 -0500

    imported svg for production

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import github from "../img/github.svg";
+import linkedIn from "../img/linkedIn.svg"
+
 const styles = {
   IconStyle: {
     height: '4rem',
@@ -15,8 +18,8 @@ export default function Footer() {
     {/* icons are brought in from the img folder in src. They are svg's */}
       <div style={styles.containerStyle}>
         {/* links are set to open in another page */}
-        <a href="https://github.com/Ch40s1" target="blank"><img src="src/img/github.svg" alt="GitHub Icon" style={styles.IconStyle} /></a>
-        <a href="https://www.linkedin.com/in/daniel-zarate-66816113b/" target="blank"> <img src="src/img/linkedIn.svg" alt="Linked-In Icon" style={styles.IconStyle} /></a>
+        <a href="https://github.com/Ch40s1" target="blank"><img src={github} alt="GitHub Icon" style={styles.IconStyle} /></a>
+        <a href="https://www.linkedin.com/in/daniel-zarate-66816113b/" target="blank"> <img src={linkedIn} alt="Linked-In Icon" style={styles.IconStyle} /></a>
       </div>
     </>
   );
